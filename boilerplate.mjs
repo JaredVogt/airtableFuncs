4c56f7538db2b7ce3675ccb7a8f5af48c870382d
Jared Vogt <jared.vogt@gmail.com>
2024-04-21 20:59:40 -0700

    adding boilerplate.mjs

---
+++ boilerplate.mjs
@@ -0,0 +1,13 @@
+// FIXME Enter the name of the automation here and any relevant description 
+
+const P = console.log
+const T = console.table
+
+// Airtable in/out functions
+const I = input.config()  // get data from previous automation
+const O = output.set()  // create data for next automation
+const baseUrl = 'https://hooks.airtable.com/workflows/v1/genericWebhook/'  // append specific webhook
+
+// SMS specific
+const message = I.message.toLocaleLowerCase() // this will be the SMS message from the router
+const keyword = message.split(' ')[0]  // only get the first word to lookup in keywords
